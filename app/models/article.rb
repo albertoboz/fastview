6ae36d41ddd44e9402cf0552778dcc889560e087
Alberto Chiorboli <alberto.chiorboli@gmail.com>
2017-04-05 14:54:56 +0200

    add search form

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  def self.search(search_term)
+    if Rails.env == 'production'
+      Article.where("title ilike ?", "%#{search_term}%")
+    else
+      Article.where("title LIKE ?", "%#{search_term}%")
+    end
+  end
 end
